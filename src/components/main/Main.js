69269f7b5f6633f81f53d14d39c1849a9a0c96d4
David Moraru <louiseflbdv@gmail.com>
2021-08-04 13:21:46 +0300

    added project structure

---
+++ src/components/main/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Main.css";
+
+const Main = () => {
+    return(
+        <main className={'main'}>
+            <div className="content">
+
+            </div>
+        </main>
+    );
+}
+
+export default Main;
---
+++ src/components/sidebar/SideBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./SideBar.css"
+const SideBar = () => {
+    return (
+        <div className={"sideBar"}>
+        </div>
+    );
+}
+
+export default SideBar;
