91d8786f733bf9ca03f96bf88e048d4962eb15ee
David Moraru <louiseflbdv@gmail.com>
2021-08-04 14:12:24 +0300

    added item for side Bar

--- src/components/sidebar/SideBar.js
+++ src/components/sidebar/SideBar.js
@@ -1,8 +1,10 @@
 import React from "react";
 import classes from "./SideBar.module.css"
+import SideBarItem from "./item/SideBarItem";
 const SideBar = () => {
     return (
         <div className={classes.sideBar}>
+            <SideBarItem/>
         </div>
     );
 }
---
+++ src/components/sidebar/item/SideBarItem.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import classes from "./SideBarItem.module.css"
+const SideBarItem = () =>{
+    return(
+        <div className={classes.item}>
+            asdsdasd
+        </div>
+    )
+}
+
+export default SideBarItem;
